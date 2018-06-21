ff40e9c3359d1947184885475636aa7182add518
91jack <2764132626@qq.com>
2018-06-22 01:37:18 +0800

    11
    11

--- static/js/common.js
+++ static/js/common.js
@@ -2,17 +2,23 @@
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
 	var deviceWidth = document.documentElement.clientWidth;
-	console.log(deviceWidth)
+	var deviceHeight = document.documentElement.clientHeight;
+//	alert(deviceWidth)
+//	alert(deviceHeight)
 		// 默认1920*1080
 		document.documentElement.style.fontSize = deviceWidth / 160 + 'px';
+		document.body.style.width = deviceWidth/12 + 'rem';
+		document.body.style.height = deviceHeight/12 + 'rem';
 		
 	   }, false);
 	   
 	window.onresize = function(){
 		var deviceWidth = document.documentElement.clientWidth;
+		var deviceHeight = document.documentElement.clientHeight;
 		
 			document.documentElement.style.fontSize = deviceWidth / 160+ 'px';
-		
+			document.body.style.width = deviceWidth/12 + 'rem';
+		document.body.style.height = deviceHeight/12 + 'rem';
 	};
 })();
 
