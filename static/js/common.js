3f9b1726bfcf2a9c0851a3755e321d7411e2dfda
91jack <2764132626@qq.com>
2018-07-19 17:28:56 +0800

    11
    11

--- static/js/common.js
+++ static/js/common.js
@@ -4,21 +4,12 @@
 	var deviceWidth = document.documentElement.clientWidth;
 	var deviceHeight = document.documentElement.clientHeight;
 			document.documentElement.style.fontSize = deviceWidth / 96 + 'px';
-			console.log(deviceWidth)
-			console.log(deviceWidth / 20)
-			document.body.style.width = deviceWidth/20 + 'rem';
-			document.body.style.height = deviceHeight/20 + 'rem';
-		
-		
 	   }, false);
 	   
 	window.onresize = function(){
 		var deviceWidth = document.documentElement.clientWidth;
 		var deviceHeight = document.documentElement.clientHeight;
-		
 			document.documentElement.style.fontSize = deviceWidth / 96+ 'px';
-			document.body.style.width = deviceWidth/20 + 'rem';
-		document.body.style.height = deviceHeight/20 + 'rem';
 	};
 })();
 
