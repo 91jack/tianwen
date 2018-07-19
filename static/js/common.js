76d1ad1f4d295e2a230988566b29b6d9cab4ddac
91jack <2764132626@qq.com>
2018-07-19 15:05:08 +0800

    1
    1

--- static/js/common.js
+++ static/js/common.js
@@ -3,12 +3,12 @@
   document.addEventListener('DOMContentLoaded', function () {
 	var deviceWidth = document.documentElement.clientWidth;
 	var deviceHeight = document.documentElement.clientHeight;
-//	alert(deviceWidth)
-//	alert(deviceHeight)
-		// 默认1920*1080
-		document.documentElement.style.fontSize = deviceWidth / 160 + 'px';
-		document.body.style.width = deviceWidth/12 + 'rem';
-		document.body.style.height = deviceHeight/12 + 'rem';
+			document.documentElement.style.fontSize = deviceWidth / 96 + 'px';
+			console.log(deviceWidth)
+			console.log(deviceWidth / 20)
+			document.body.style.width = deviceWidth/20 + 'rem';
+			document.body.style.height = deviceHeight/20 + 'rem';
+		
 		
 	   }, false);
 	   
@@ -16,9 +16,9 @@
 		var deviceWidth = document.documentElement.clientWidth;
 		var deviceHeight = document.documentElement.clientHeight;
 		
-			document.documentElement.style.fontSize = deviceWidth / 160+ 'px';
-			document.body.style.width = deviceWidth/12 + 'rem';
-		document.body.style.height = deviceHeight/12 + 'rem';
+			document.documentElement.style.fontSize = deviceWidth / 96+ 'px';
+			document.body.style.width = deviceWidth/20 + 'rem';
+		document.body.style.height = deviceHeight/20 + 'rem';
 	};
 })();
 
